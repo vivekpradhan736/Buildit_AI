fb5add0d805ece48b42a6ce9dc5a7ac5ff253d20
Vivek <vivekpradhan736@gmail.com>
2025-02-01 21:43:46 +0530

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Required for Netlify static hosting
-  trailingSlash: true, // Ensures correct routing
   images: {
     domains: ["lh3.googleusercontent.com"],
-    unoptimized: true, // Required for images to work correctly
   },
 };
 
